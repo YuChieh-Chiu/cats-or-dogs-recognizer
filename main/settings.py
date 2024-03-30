8547511d1421c5614395463f4ef33efc90bdb7fb
YuChieh-Chiu <81817517+YuChieh-Chiu@users.noreply.github.com>
2024-03-31 00:52:45 +0800

    v1.0.5

--- main/settings.py
+++ main/settings.py
@@ -79,9 +79,7 @@ WSGI_APPLICATION = "main.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        dj_database_url.config()
-    }
+    "default": dj_database_url.config()
 }
 
 # Password validation
