5999080839c716d2bafa37e94b1f62d1f11877cb
YuChieh-Chiu <81817517+YuChieh-Chiu@users.noreply.github.com>
2024-03-31 00:14:45 +0800

    v1.0.1

--- main/settings.py
+++ main/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -78,11 +79,12 @@ WSGI_APPLICATION = "main.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        dj_database_url.config(
+            'postgresql://postgres:postgres@localhost:5432/mysite',
+            conn_max_age=600    
+    )
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
