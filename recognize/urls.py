58bcf735d897d7371ac25e16bef591bd6d169cb3
YuChieh-Chiu <81817517+YuChieh-Chiu@users.noreply.github.com>
2024-04-14 00:51:14 +0800

    v1.3.0 - add AJAX

--- recognize/urls.py
+++ recognize/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views # 引用這個資料夾中的views檔案
 
 urlpatterns = [
-    path("", views.recognizer, name="recognize")
+    path("", views.recognizer, name="recognize"),
+    path("result", views.ajax_model_predict, name="predict_result")
 ]
